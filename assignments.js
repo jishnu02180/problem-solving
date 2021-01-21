4583bd5ef794d3b64892a61598e7cdd70bdf3481
Jishnu Sarkar <jishnu.cse7th@gmail.com>
2021-01-22 01:19:29 +0600

    code refactor

--- assignments.js
+++ assignments.js
@@ -4,44 +4,40 @@ function kilometerToMeter(value) {
         return meter;
     }
     else {
-        return 'this is a negative value';
+        return 'please input the positive value';
     }
 }
 
 var results = kilometerToMeter(2);
 //console.log(results);
 
-function budgetCalculator(watch, phone, laptop){
-    
-    if(watch > 0 && phone > 0 && laptop > 0){
+function budgetCalculator(watch, phone, laptop) {
+    if (watch > 0 && phone > 0 && laptop > 0) {
         var total = watch * 50 + phone * 100 + laptop * 500;
         return total;
-    } else{
+    } else {
         return 'please input the positive value';
     }
-
 }
 
-var totalCost = budgetCalculator(3,-5,2);
+var totalCost = budgetCalculator(3, -5, 2);
 //console.log(totalCost);
 
-function hotelCost(value){
-
+function hotelCost(value) {
     var totalCost = 0;
-
-    if(value > 0){
-        if(value <= 10){
+    if (value > 0) {
+        if (value <= 10) {
             return totalCost = value * 100;
-        } else if(value <= 20){
-            var first10Days = 10 * 100;
+        } else if (value <= 20) {
+            var firstTenDays = 10 * 100;
             var remainngDays = value - 10;
-            var last10Days = remainngDays * 80;
-            totalCost = first10Days + last10Days;
+            var lastTenDays = remainngDays * 80;
+            totalCost = firstTenDays + lastTenDays;
             return totalCost;
 
-        } else{
+        } else {
             var first_10_days = 10 * 100;
-            var last_10_days =  10 * 80;
+            var last_10_days = 10 * 80;
             var remainsDays = value - 20;
             var lastRemainDays = remainsDays * 50;
             totalCost = first_10_days + last_10_days + lastRemainDays;
@@ -53,23 +49,25 @@ function hotelCost(value){
 
 }
 
-var holetTotalCost = hotelCost(2);
+var holetTotalCost = hotelCost(12);
 //console.log(holetTotalCost);
 
-function megaFriend(friends){
-   
-    var largeValue = [0];
-    for(var i = 0; i < friends.length; i++){
-        var element = friends[i];
-        if(element.length > largeValue.length){
-            largeValue = element;
+function megaFriend(friends) {
+    var longName = [0];
+    if (friends.length > 0) {
+        for (var i = 0; i < friends.length; i++) {
+            var element = friends[i];
+            if (element.length > longName.length) {
+                longName = element;
+            }
         }
+        return longName;
+    } else {
+        return 'Please input a valid array';
     }
-    return largeValue;
-
 }
 
-var friends = ['kamal','jamalvai', 'jishnu','JishnuSardar'];
+var friends = ["jishnu","sardar"];
 var results = megaFriend(friends);
 console.log(results);
 
